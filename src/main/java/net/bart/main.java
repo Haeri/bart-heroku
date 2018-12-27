c6e29dbdc75d1e88cb4238c6e4ca0bed524c1438
Haeri <Haeri@users.noreply.github.com>
2018-12-27 11:04:52 +0100

    added file upload

--- src/main/java/net/bart/main.java
+++ src/main/java/net/bart/main.java
@@ -3,38 +3,79 @@ package net.bart;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.tzemp.config.Config;
-import com.github.tzemp.config.ParsingRule;
 import com.github.tzemp.parser.Parser;
 import com.github.tzemp.parser.ParserSummary;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.ObjectUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
+
+import spark.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.nio.file.*;
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.*;
 
 public class main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        enableDebugScreen();
 
-        Config c = readConfig();
-        List<String> lines = new ArrayList<String>();
+        File uploadDir = new File("upload");
+        uploadDir.mkdir(); // create the upload directory if it doesn't exist
 
-        try {
-            lines = FileUtils.readLines(new File("src/main/resources/logs/20181213-183200.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        staticFiles.externalLocation("upload");
 
-        Parser p = new Parser(lines, c);
-        ParserSummary ps = p.getParserSummary();
+        get("/", (req, res) ->
+                "<form method='post' enctype='multipart/form-data'>" // note the enctype
+                        + "    <input type='file' name='uploaded_file' accept='.png'>" // make sure to call getPart using the same "name" in the post
+                        + "    <button>Upload picture</button>"
+                        + "</form>"
+        );
 
-        System.out.println("Build Status: " + ps.getBuildStatus());
-        System.out.println("Error cause: " + ps.getErrorCause());
-        System.out.println("Question: " + ps.getBestQuestion());
+        post("/", (req, res) -> {
+
+            Path tempFile = Files.createTempFile(uploadDir.toPath(), "", "");
+
+            req.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+
+            try (InputStream input = req.raw().getPart("uploaded_file").getInputStream()) { // getPart needs to use same "name" as input field in form
+                Files.copy(input, tempFile, StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            logInfo(req, tempFile);
+
+
+
+
+
+
+            System.out.println("Hello World!");
+
+            Config c = readConfig();
+            List<String> lines = new ArrayList<String>();
+
+            try {
+                lines = FileUtils.readLines(new File("src/main/resources/logs/20181213-183200.txt"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parser p = new Parser(lines, c);
+            ParserSummary ps = p.getParserSummary();
+
+            System.out.println("Build Status: " + ps.getBuildStatus());
+            System.out.println("Error cause: " + ps.getErrorCause());
+            System.out.println("Question: " + ps.getBestQuestion());
+
+
+            return "<h1>You uploaded this image:<h1><img src='" + tempFile.getFileName() + "'>";
+
+        });
     }
 
     private static List<String> readLogs(String filePath){
@@ -63,6 +104,21 @@ public class main {
             e.printStackTrace();
         }
         System.out.println("END READING CONFIG - FAILURE");
+        return null;
+    }
+
+
+    // methods used for logging
+    private static void logInfo(Request req, Path tempFile) throws IOException, ServletException {
+        System.out.println("Uploaded file '" + getFileName(req.raw().getPart("uploaded_file")) + "' saved as '" + tempFile.toAbsolutePath() + "'");
+    }
+
+    private static String getFileName(Part part) {
+        for (String cd : part.getHeader("content-disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                return cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
         return null;
     }
 }
